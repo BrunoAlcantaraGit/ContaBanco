9910be0a6aa23018034193d1a56c2164878c92ec
Bruno Alcantara <120646838+BrunoAlcantaraGit@users.noreply.github.com>
2023-12-22 08:44:43 -0300

    adcionando entradas

--- src/Banco.java
+++ src/Banco.java
@@ -1,10 +1,10 @@
 import java.math.BigDecimal;
 
 public class Banco {
-int numero;
-String agencia;
-String nomeCliente;
-BigDecimal saldo;
+    int numero;
+    String agencia;
+    String nomeCliente;
+    BigDecimal saldo;
 
 
     public Banco() {
@@ -12,41 +12,24 @@ BigDecimal saldo;
     }
 
 
-    public void imprime(){
-       System.out.println("Olá, " + nomeCliente + ". Obrigado por criar uma conta em nosso banco!" +
-               " Sua agência é " + agencia+ ",conta: "+ numero + ", e seu saldo: " + saldo + ". Já está disponível para saque.");
-   }
-
-
-    public int getNumero() {
-        return numero;
+    public void imprime() {
+        System.out.println("Olá, " + nomeCliente + ". Obrigado por criar uma conta em nosso banco!" +
+                " Sua agência é " + agencia + ",conta: " + numero + ", seu saldo é de:: " + saldo + ". Já está disponível para saque.");
     }
 
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
-    public String getAgencia() {
-        return agencia;
-    }
-
     public void setAgencia(String agencia) {
         this.agencia = agencia;
-    }
-
-    public String getNomeCliente() {
-        return nomeCliente;
     }
 
     public void setNomeCliente(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
 
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
-
     public void setSaldo(BigDecimal saldo) {
-        saldo = saldo;
+        this.saldo = saldo;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
         Banco banco = new Banco();
 
         System.out.println("Informe seu nome !");
+        banco.setNomeCliente(scanner.next());
+        System.out.println("informe a agencia");
         banco.setAgencia(scanner.next());
-        System.out.println("agencia");
-        banco.setAgencia(scanner.next());
-        System.out.println("numero da conta");
+        System.out.println("informe o numero da conta");
         banco.setNumero(scanner.nextInt());
-
-
+        System.out.println(" informe seu saldo");
+        banco.setSaldo(scanner.nextBigDecimal());
         banco.imprime();
     }
 }
